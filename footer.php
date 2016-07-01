6db7496c272ba909ae9837ced9dbe71abe873351
dorzki <me@dorzki.co.il>
2016-07-01 13:26:07 +0300

    Added template parts and restructured footer.php.

--- footer.php
+++ footer.php
@@ -1,39 +1,23 @@
-<section id="footer">
-	<div class="inner">
-		<h2 class="major"><?php _e( 'Get in touch', 'solid-state' ); ?></h2>
-		<p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
-		<form method="post" action="#">
-			<div class="field">
-				<label for="name"><?php _e( 'Name', 'solid-state' ); ?></label>
-				<input type="text" name="name" id="name" />
+		<section id="footer">
+			<div class="inner">
+				<h2 class="major"><?php _e( 'Get in touch', 'solid-state' ); ?></h2>
+				<p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
+
+				<!-- CONTACT FORM -->
+				<?php get_template_part( 'templates/contact', 'form' ); ?>
+				<!-- /CONTACT FORM -->
+
+				<!-- CONTACT DETAILS -->
+				<?php get_template_part( 'templates/contact', 'details' ); ?>
+				<!-- /CONTACT DETAILS -->
+
+				<ul class="copyright">
+					<li>&copy; <?php bloginfo( 'name' ); ?> <?php _e( 'All rights reserved.' , 'solid-state' ); ?></li>
+					<li><?php _e( 'Design:', 'solid-state' ); ?> <a href="http://html5up.net"><?php _e( 'HTML5 UP', 'solid-state' ); ?></a></li>
+				</ul>
 			</div>
-			<div class="field">
-				<label for="email"><?php _e( 'Email', 'solid-state' ); ?></label>
-				<input type="email" name="email" id="email" />
-			</div>
-			<div class="field">
-				<label for="message"><? _e( 'Message', 'solid-state' ); ?></label>
-				<textarea name="message" id="message" rows="4"></textarea>
-			</div>
-			<ul class="actions">
-				<li><input type="submit" value="<?php _e( 'Send Message', 'solid-state' ); ?>" /></li>
-			</ul>
-		</form>
-		<ul class="contact">
-			<li class="fa-home">
-				Untitled Inc<br />
-				1234 Somewhere Road Suite #2894<br />
-				Nashville, TN 00000-0000
-			</li>
-			<li class="fa-phone">(000) 000-0000</li>
-			<li class="fa-envelope"><a href="#">information@untitled.tld</a></li>
-			<li class="fa-twitter"><a href="#">twitter.com/untitled-tld</a></li>
-			<li class="fa-facebook"><a href="#">facebook.com/untitled-tld</a></li>
-			<li class="fa-instagram"><a href="#">instagram.com/untitled-tld</a></li>
-		</ul>
-		<ul class="copyright">
-			<li>&copy; <?php bloginfo( 'name' ); ?> <?php _e( 'All rights reserved.' , 'solid-state' ); ?></li>
-			<li><?php _e( 'Design:', 'solid-state' ); ?> <a href="http://html5up.net"><?php _e( 'HTML5 UP', 'solid-state' ); ?></a></li>
-		</ul>
+		</section>
 	</div>
-</section>
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ templates/contact-details.php
@@ -0,0 +1,12 @@
+<ul class="contact">
+	<li class="fa-home">
+		Untitled Inc<br />
+		1234 Somewhere Road Suite #2894<br />
+		Nashville, TN 00000-0000
+	</li>
+	<li class="fa-phone">(000) 000-0000</li>
+	<li class="fa-envelope"><a href="#">information@untitled.tld</a></li>
+	<li class="fa-twitter"><a href="#">twitter.com/untitled-tld</a></li>
+	<li class="fa-facebook"><a href="#">facebook.com/untitled-tld</a></li>
+	<li class="fa-instagram"><a href="#">instagram.com/untitled-tld</a></li>
+</ul>
